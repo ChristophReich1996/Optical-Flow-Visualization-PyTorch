2f3f7cc2d7c975408493ce16da303e1a3d9b21dd
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-08-12 15:18:05 +0200

    Debug

--- flow_vis_torch/flow_vis_torch.py
+++ flow_vis_torch/flow_vis_torch.py
@@ -137,7 +137,6 @@ def flow_to_color(flow: torch.Tensor, clip_flow: Optional[Union[float, torch.Ten
     # Init flow image
     flow_image = torch.zeros(batch_size, 3, height, width, device=device)
     # Iterate over batch dimension
-    print(flow_vertical.shape)
     for index in range(batch_size):
         flow_image[index] = _flow_hw_to_color(flow_vertical=flow_vertical[index],
                                               flow_horizontal=flow_horizontal[index], color_wheel=color_wheel,
